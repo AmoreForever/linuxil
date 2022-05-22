cbaeaf85e12a38bf06502613e18cd5932daa33f3
AmoreForever <103936585+AmoreForever@users.noreply.github.com>
2022-05-22 16:00:17 +0500

    Update backend.py

--- friendly-telegram/database/backend.py
+++ friendly-telegram/database/backend.py
@@ -1,237 +1,79 @@
-import asyncio
+# █ █ ▀ █▄▀ ▄▀█ █▀█ ▀    ▄▀█ ▀█▀ ▄▀█ █▀▄▀█ ▄▀█
+# █▀█ █ █ █ █▀█ █▀▄ █ ▄  █▀█  █  █▀█ █ ▀ █ █▀█
+#
+#              © Copyright 2022
+#
+#          https://t.me/hikariatama
+#
+# 🔒 Licensed under the GNU GPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
+# meta developer: @hikariatama
+
 import logging
 
-import telethon
-from telethon.errors.rpcerrorlist import (
-    MessageEditTimeExpiredError,
-    MessageNotModifiedError,
-)
-from telethon.tl.custom import Message as CustomMessage
-from telethon.tl.functions.channels import CreateChannelRequest, DeleteChannelRequest
-from telethon.tl.types import Message
-import json
-import os
+from telethon.tl.functions.channels import EditAdminRequest, InviteToChannelRequest
+from telethon.tl.types import ChatAdminRights
 
-from .. import main, utils
-
-ORIGIN = "/".join(main.__file__.split("/")[:-2])
-
-logger = logging.getLogger(__name__)
+from .. import loader, utils
 
 
-class CloudBackend:
-    def __init__(self, client):
+@loader.tds
+class HikkaLoggerMod(loader.Module):
+    """Transfer your Hikka logs to classic bot"""
+
+    strings = {"name": "AmoreLogger"}
+
+    async def client_ready(self, client, db):
         self._client = client
-        self._me = None
-        self.db = None
-        self._assets = None
-        self._anti_double_lock = asyncio.Lock()
-        self._anti_double_asset_lock = asyncio.Lock()
-        self._data_already_exists = False
-        self._assets_already_exists = False
-        self.close = lambda: None
+        self._db = db
 
-    async def init(self, trigger_refresh):
-        self._me = await self._client.get_me(True)
-        self._db_path = os.path.join(ORIGIN, f"config-{self._me.user_id}.json")
-        self._callback = trigger_refresh
-
-    async def _find_data_channel(self):
-        async for dialog in self._client.iter_dialogs(None, ignore_migrated=True):
-            if dialog.name == f"linuxil-{self._me.user_id}-data" and dialog.is_channel:
-                members = await self._client.get_participants(dialog, limit=2)
-                if len(members) != 1:
-                    continue
-                logger.debug(f"Found data chat! It is {dialog.id}.")
-                return dialog.entity
-
-    async def _make_data_channel(self):
-        async with self._anti_double_lock:
-            if self._data_already_exists:
-                return await self._find_data_channel()
-            self._data_already_exists = True
-            return (
-                await self._client(
-                    CreateChannelRequest(
-                        f"linuxil-{self._me.user_id}-data",
-                        "// Не трогай",
-                        megagroup=True,
-                    )
-                )
-            ).chats[0]
-
-    async def _find_asset_channel(self):
-        async for dialog in self._client.iter_dialogs(None, ignore_migrated=True):
-            if (
-                dialog.name == f"linuxil-{self._me.user_id}-assets"
-                and dialog.is_channel
-            ):
-                members = await self._client.get_participants(dialog, limit=2)
-                if len(members) != 1:
-                    continue
-                logger.debug(f"Found asset chat! It is {dialog.id}.")
-                return dialog.entity
-
-    async def _make_asset_channel(self):
-        async with self._anti_double_asset_lock:
-            if self._assets_already_exists:
-                return await self._find_data_channel()
-            self._assets_already_exists = True
-            return (
-                await self._client(
-                    CreateChannelRequest(
-                        f"linuxil-{self._me.user_id}-assets",
-                        "// Не трогай",
-                        megagroup=True,
-                    )
-                )
-            ).chats[0]
-
-    async def do_download(self, force_from_data_channel=False):
-        """
-        Attempt to download the database.
-        Return the database (as unparsed JSON) or None
-        """
-        if main.get_config_key("use_file_db") and not force_from_data_channel:
-            try:
-                with open(self._db_path, "r", encoding="utf-8") as f:
-                    data = json.dumps(json.loads(f.read()))
-            except Exception:
-                data = await self.do_download(force_from_data_channel=True)
-                await self.do_upload(data)
-
-            return data
-
-        if not self.db:
-            self.db = await self._find_data_channel()
-
-            if not self.db:
-                logging.debug("No DB, returning")
-                return None
-
-            self._client.add_event_handler(
-                self._callback,
-                telethon.events.messageedited.MessageEdited(chats=[self.db]),
-            )
-
-        msgs = self._client.iter_messages(entity=self.db, reverse=True)
-
-        data = ""
-        lastdata = ""
-
-        async for msg in msgs:
-            if isinstance(msg, Message):
-                data += lastdata
-                lastdata = msg.message
-            else:
-                logger.debug(f"Found service message {msg}")
-
-        return data
-
-    async def do_upload(self, data):
-        """
-        Attempt to upload the database.
-        Return True or throw
-        """
-
-        if main.get_config_key("use_file_db"):
-            try:
-                with open(self._db_path, "w", encoding="utf-8") as f:
-                    f.write(data or "{}")
-            except Exception:
-                logger.exception("Database save failed!")
-                raise
-
-            return True
-
-        if not self.db:
-            self.db = await self._find_data_channel()
-            if not self.db:
-                self.db = await self._make_data_channel()
-
-            self._client.add_event_handler(
-                self._callback,
-                telethon.events.messageedited.MessageEdited(chats=[self.db]),
-            )
-
-        msgs = await self._client.get_messages(entity=self.db, reverse=True)
-
-        ops = []
-        sdata = data
-        newmsg = False
-        for msg in msgs:
-            if isinstance(msg, Message):
-                if len(sdata):
-                    if msg.id == msgs[-1].id:
-                        newmsg = True
-                    if sdata[:4096] != msg.message:
-                        ops += [
-                            msg.edit(f"<code>{utils.escape_html(sdata[:4096])}</code>")
-                        ]
-                    sdata = sdata[4096:]
-                elif msg.id != msgs[-1].id:
-                    ops += [msg.delete()]
-
-        if await self._do_ops(ops):
-            return await self.do_upload(data)
-
-        while len(
-            sdata
-        ):  # Only happens if newmsg is True or there was no message before
-            newmsg = True
-            await self._client.send_message(self.db, utils.escape_html(sdata[:4096]))
-            sdata = sdata[4096:]
-
-        if newmsg:
-            await self._client.send_message(self.db, "🥷Пожалуйста не трогайте группу логов")
-
-        return True
-
-    async def _do_ops(self, ops):
-        try:
-            for r in await asyncio.gather(*ops, return_exceptions=True):
-                if isinstance(r, MessageNotModifiedError):
-                    logging.debug("db not modified", exc_info=r)
-                elif isinstance(r, Exception):
-                    raise r  # Makes more sense to raise even for MessageEditTimeExpiredError
-                elif not isinstance(r, Message):
-                    logging.debug("unknown ret from gather, %r", r)
-        except MessageEditTimeExpiredError:
-            logging.debug("Making new channel.")
-            _db = self.db
-            self.db = None
-            await self._client(DeleteChannelRequest(channel=_db))
-            return True
-
-        return False
-
-    async def store_asset(self, message):
-        if not self._assets:
-            self._assets = await self._find_asset_channel()
-
-        if not self._assets:
-            self._assets = await self._make_asset_channel()
-
-        return (
-            (await self._client.send_message(self._assets, message)).id
-            if isinstance(message, (Message, CustomMessage))
-            else (
-                await self._client.send_message(
-                    self._assets, file=message, force_document=True
-                )
-            ).id
+        chat, is_new = await utils.asset_channel(
+            self._client,
+            "linuxil-logs",
+            "🌌 Your Linuxil logs will appear in this chat",
+            silent=True,
         )
 
-    async def fetch_asset(self, id_):
-        if not self._assets:
-            self._assets = await self._find_asset_channel()
+        self._logchat = int(f"-100{chat.id}")
 
-        if not self._assets:
-            return None
+        logger = logging.getLogger(__name__)
 
-        ret = await self._client.get_messages(self._assets, ids=[id_])
+        if not is_new or all(
+            participant.id != self.inline.bot_id
+            for participant in (await self._client.get_participants(chat))
+        ):
+            logging.getLogger().handlers[0].install_tg_log(self)
+            logger.debug(f"Bot logging installed for {self._logchat}")
+            return
 
-        if not ret:
-            return None
+        logger.debug("New logging chat created, init setup...")
 
-        return ret[0]
+        try:
+            await self._client(InviteToChannelRequest(chat, [self.inline.bot_username]))
+        except Exception:
+            logger.warning("Unable to invite logger to chat")
+
+        try:
+            await self._client(
+                EditAdminRequest(
+                    channel=chat,
+                    user_id=self.inline.bot_username,
+                    admin_rights=ChatAdminRights(ban_users=True),
+                    rank="Logger",
+                )
+            )
+        except Exception:
+            pass
+
+        try:
+            await utils.set_avatar(
+                self._client,
+                chat,
+                "https://i.imgur.com/MWoMKp0.jpeg",
+            )
+        except Exception:
+            pass
+
+        logging.getLogger().handlers[0].install_tg_log(self)
+        logger.debug(f"Bot logging installed for {self._logchat}")
